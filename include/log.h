8a44f058fe76efc3a013903470d752b71268a015
HuJJ <hduhujiajun@outlook.com>
2022-11-16 10:53:20 +0800

    refactor(output): add `yyerror()` in include/log.h

---
+++ include/log.h
@@ -0,0 +1,18 @@
+#ifndef __LOG__
+#define __LOG__
+
+#include <stdio.h>
+
+/* In stdio.h
+ * Write formatted output to STREAM.
+ * This function is a possible cancellation point and therefore not
+ * marked with __THROW.
+ * ```
+ * extern int fprintf (FILE *__restrict __stream,
+ *                     const char *__restrict __format, ...);
+ * ```
+ * Write formatted output to stdout.
+ */
+#define yyerror(fmt, ...) fprintf(stderr, fmt"\n", ##__VA_ARGS__)
+
+#endif
