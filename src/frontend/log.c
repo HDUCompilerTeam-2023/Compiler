407d57e56ebd865157eab6a47ee24d87b55912a1
HuJJ <hduhujiajun@outlook.com>
2022-11-24 16:18:02 +0800

    feat(output): more clear output

--- src/frontend/log.c
+++ src/frontend/log.c
@@ -5,20 +5,29 @@ int yylog(loglevel level, YYLTYPE *loc, yyscan_t scanner, const char *format, ..
     int ret;
     FILE *output = stdout;
 
+    if (level == err) {
+        output = stderr;
+    }
+
     switch (level) {
     case err:
-        output = stderr;
-        fprintf(output, "[error] ");
-    case debug:
-#ifndef DEBUG
-        ret = 0;
+        fprintf(output, "[Error] ");
         break;
-#endif
     case log:
-        va_start(args, format);
-        ret = vfprintf(output, format, args);
-        va_end(args);
-        fprintf(output, "\n");
-    };
+        fprintf(output, "[Trace] ");
+        break;
+    case debug:
+        fprintf(output, "[Debug] ");
+        break;
+    }
+
+    if (loc) {
+        fprintf(output, "At line %d, ", loc->first_line);
+    }
+
+    va_start(args, format);
+    ret = vfprintf(output, format, args);
+    va_end(args);
+    fprintf(output, "\n");
     return ret;
 }
