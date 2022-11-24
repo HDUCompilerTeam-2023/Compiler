e743f94e835bb5680f656ff29ab66fe9d5db77c0
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2022-11-24 21:06:15 +0800

    feat(parser): return root of syntax tree

--- include/frontend/log.h
+++ include/frontend/log.h
@@ -13,7 +13,7 @@ typedef enum {
 
 int yylog(loglevel level, YYLTYPE *loc, yyscan_t scanner, const char *format, ...);
 
-#define yyerror(loc, scan, fmt, ...) yylog(err, loc, scan, fmt, ##__VA_ARGS__)
+#define yyerror(loc, scan, root, fmt, ...) yylog(err, loc, scan, fmt, ##__VA_ARGS__)
 #define yydebug(loc, scan, fmt, ...) yylog(debug, loc, scan, fmt, ##__VA_ARGS__)
 
 #endif
