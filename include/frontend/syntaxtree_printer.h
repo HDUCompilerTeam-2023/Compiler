505305e9e75747a2df5df16e29becf262b26a688
HuJJ <hduhujiajun@outlook.com>
2022-11-25 01:49:47 +0800

    feat(parser): print syntax tree

---
+++ include/frontend/syntaxtree_printer.h
@@ -0,0 +1,8 @@
+#ifndef __FRONTEND_SYNTAXTREE_PRINTER__
+#define __FRONTEND_SYNTAXTREE_PRINTER__
+
+#include <frontend/syntaxtree.h>
+
+void frontend_print_syntaxtree(pCompUnitNode CompUnit);
+
+#endif
