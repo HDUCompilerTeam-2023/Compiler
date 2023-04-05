5f9bb58c39dda933bd1e282077c922c8539fc508
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-04-05 18:22:04 +0800

    fix(output): add vaLOG
    can not correct trans args

--- include/util/log.h
+++ include/util/log.h
@@ -1,6 +1,9 @@
 #ifndef __UTIL_LOG__
 #define __UTIL_LOG__
 
+#include <stdarg.h>
+
 int LOG(const char *tag, const char *pos, const char *format, ...);
+int vaLOG(const char *tag, const char *pos, const char *format, __gnuc_va_list va_list);
 
 #endif
