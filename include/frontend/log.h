9298c0270e49d821a618d5b67cfb6982b48006e4
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-04-05 18:44:07 +0800

    feat(output): output trace message as debug level

--- include/frontend/log.h
+++ include/frontend/log.h
@@ -8,6 +8,7 @@
 void frontend_log(loglevel level, YYLTYPE *loc, yyscan_t scanner, const char *format, ...);
 
 #define yyerror(loc, scan, fmt, ...) frontend_log(error, loc, scan, fmt, ##__VA_ARGS__)
-#define yylog(loc, scan, fmt, ...) frontend_log(log, loc, scan, fmt, ##__VA_ARGS__)
+#define yyinfo(loc, scan, fmt, ...) frontend_log(info, loc, scan, fmt, ##__VA_ARGS__)
+#define yydebug(loc, scan, fmt, ...) frontend_log(debug, loc, scan, fmt, ##__VA_ARGS__)
 
 #endif
