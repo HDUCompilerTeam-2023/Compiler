c0d52158bba68b7fc76dac00910d41c86e84e79a
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-04-07 09:37:45 +0800

    build: add rule to build header of lexer

--- script/lex.mk
+++ script/lex.mk
@@ -1,7 +1,7 @@
 # Lex rules
 LEXFLAGS +=
 
-$(TMP_DIR)/%.yy.c: %.l
+$(TMP_DIR)/%.yy.h $(TMP_DIR)/%.yy.c: %.l
 	@echo '+ LEX $<'
 	@mkdir -p $(dir $@)
 	@$(LEX) $(LEXFLAGS) --header-file=$(@:%.c=%.h) -o $@ $<
