8786d135533da4d71994ff211add7e2dea931665
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-04-10 14:48:15 +0800

    style: move symbol table struct to hir/symbol.h

--- include/hir/symbol.h
+++ include/hir/symbol.h
@@ -3,6 +3,43 @@
 
 #include <hir.h>
 
+typedef struct symbol_item symbol_item, *p_symbol_item;
+typedef struct symbol_name symbol_name, *p_symbol_name;
+typedef struct symbol_table symbol_table, *p_symbol_table;
+
+struct symbol_item {
+    p_symbol_name p_name;
+    p_symbol_item p_prev;
+
+    uint16_t level;
+    p_symbol_item p_next;
+
+    p_symbol_sym p_info;
+};
+
+struct symbol_name {
+    char * name;
+    p_symbol_item p_item;
+
+    size_t hash_tag;
+    hlist_node node;
+};
+
+struct symbol_table {
+    p_symbol_item p_item;
+
+    p_symbol_table p_prev;
+};
+
+struct symbol_store {
+    hlist_hash hash;
+
+    p_symbol_sym p_info;
+
+    uint16_t level;
+    p_symbol_table p_top_table;
+};
+
 struct symbol_init {
     size_t size;
     p_hir_exp *memory;
