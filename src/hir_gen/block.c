73ce196275e488a99c4a4067e97d2e3b9d05df25
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-04-10 11:21:57 +0800

    feat: fold all empty block and stmt

--- src/hir_gen/block.c
+++ src/hir_gen/block.c
@@ -5,13 +5,28 @@
 p_hir_block hir_block_gen(void) {
     p_hir_block p_block = malloc(sizeof(*p_block));
     *p_block = (hir_block) {
+        .length = 0,
         .stmt = list_head_init(&p_block->stmt),
     };
     return p_block;
 }
 
 p_hir_block hir_block_add(p_hir_block p_block, p_hir_stmt p_stmt) {
-    list_add_prev(&p_stmt->node, &p_block->stmt);
+    if (p_stmt->type != hir_stmt_block) {
+        if (p_stmt->type == hir_stmt_exp && p_stmt->p_exp == NULL) {
+            free(p_stmt);
+        }
+        else {
+            ++p_block->length;
+            list_add_prev(&p_stmt->node, &p_block->stmt);
+        }
+    }
+    else {
+        p_block->length += p_stmt->p_block->length;
+        list_blk_add_prev(&p_stmt->p_block->stmt, &p_block->stmt);
+        free(p_stmt->p_block);
+        free(p_stmt);
+    }
     return p_block;
 }
 
