3b3ffdf1f0b73dc5e764fe5d36a6883166cbe48b
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-04-11 19:09:46 +0800

    feat: clear output

--- src/hir_print/symbol.c
+++ src/hir_print/symbol.c
@@ -11,9 +11,12 @@ void symbol_define_print(p_symbol_sym p_sym) {
     }
 
     if (p_sym->p_type->kind == type_func) {
-        printf("\nparam:\n");
+        printf("\n");
         p_list_head p_node;
         p_symbol_type p_param_type = p_sym->p_type->p_params;
+        if (p_param_type) {
+            printf("param:\n");
+        }
         list_for_each(p_node, &p_sym->local) {
             if (!p_param_type) break;
             symbol_init_print(list_entry(p_node, symbol_sym, node));
@@ -53,20 +56,27 @@ void symbol_init_print(p_symbol_sym p_sym) {
 
 void symbol_store_print(p_symbol_store pss) {
     p_list_head p_node;
-    printf("global:\n");
+    if (!list_head_alone(&pss->global)) {
+        printf("global:\n");
+    }
     list_for_each(p_node, &pss->global) {
         symbol_init_print(list_entry(p_node, symbol_sym, node));
     }
-    printf("\n");
 
-    printf("functions:\n");
+    if (!list_head_alone(&pss->def_function)) {
+        if (!list_head_alone(&pss->global))
+            printf("\n");
+        printf("functions:\n");
+    }
     list_for_each(p_node, &pss->def_function) {
         symbol_init_print(list_entry(p_node, symbol_sym, node));
     }
-    printf("\n");
 
-
-    printf("extern:\n");
+    if (!list_head_alone(&pss->ndef_function)) {
+        if (!list_head_alone(&pss->def_function))
+            printf("\n");
+        printf("extern:\n");
+    }
     list_for_each(p_node, &pss->ndef_function) {
         symbol_init_print(list_entry(p_node, symbol_sym, node));
     }
