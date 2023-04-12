2bc1b0747190dde8f14e28590113faaed4a949d1
zengyong <2595650269@qq.com>
2023-04-12 22:31:42 +0800

    feat: add func temp id

--- include/mir/func.h
+++ include/mir/func.h
@@ -2,8 +2,9 @@
 #define __MIRFUNC__
 #include <mir.h>
 struct mir_func{
-    p_mir_basic_block basic_block;
-    p_symbol_sym p_func;
+    p_mir_basic_block p_basic_block;
+    p_symbol_sym p_func_sym;
+    size_t temp_id; // 临时变量数量
 
     list_head node;
 };
