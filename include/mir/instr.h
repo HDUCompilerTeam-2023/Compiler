a00c83054d1531bafc34c6c9f5fe875fa61d41e9
zengyong <2595650269@qq.com>
2023-04-12 22:28:38 +0800

    refactor: bag mir_instr_type, change var_name, and add instr index

--- include/mir/instr.h
+++ include/mir/instr.h
@@ -1,42 +1,42 @@
 #ifndef __MIR_INSTR__
 #define __MIR_INSTR__
 #include <mir.h>
+enum mir_instr_type{
+    // binary instr
+    mir_add_op,
+    mir_sub_op,
+    mir_mul_op,
+    mir_div_op,
+    mir_mod_op,
+    mir_and_op,
+    mir_or_op,
+    mir_eq_op,
+    mir_neq_op,
+    mir_l_op,
+    mir_leq_op,
+    mir_g_op,
+    mir_geq_op,
 
+    // unary instr
+    mir_minus_op,
+    mir_not_op,
+    mir_int2float_op,
+    mir_float2int_op,
+    mir_assign,
+    // memory instr
+    // mir_alloca,
+    // mir_store,
+    // mir_load,
+
+    // other
+    mir_call,
+    mir_array,
+    mir_ret,
+    mir_br,
+    mir_condbr,
+};
 struct mir_instr{
-    enum{
-        // binary instr
-        mir_add_op,
-        mir_sub_op,
-        mir_mul_op,
-        mir_div_op,
-        mir_mod_op,
-        mir_and_op,
-        mir_or_op,
-        mir_eq_op,
-        mir_neq_op,
-        mir_l_op,
-        mir_leq_op,
-        mir_g_op,
-        mir_geq_op,
-
-        // unary instr
-        mir_minus_op,
-        mir_not_op,
-        mir_int2float_op,
-        mir_float2int_op,
-        mir_assign,
-        // memory instr
-        // mir_alloca,
-        // mir_store,
-        // mir_load,
-
-        // other
-        mir_call,
-        mir_array,
-        mir_ret,
-        mir_br,
-        mir_condbr,
-    }irkind;
+    mir_instr_type irkind;
     union{
         p_mir_binary_instr p_mir_binary;
         p_mir_unary_instr p_mir_unary;
@@ -46,30 +46,31 @@ struct mir_instr{
         p_mir_br_instr p_mir_br;
         p_mir_condbr_instr p_mir_condbr;
     };
+    int index; // 指令编号
 
     list_head node; // 下一条指令
 };
 
 struct mir_binary_instr{
-    p_mir_operand src1, src2;
-    p_mir_symbol des;
+    p_mir_operand p_src1, p_src2;
+    p_mir_symbol p_des;
 };
 
 struct mir_unary_instr{
-    p_mir_operand src;
-    p_mir_symbol des;
+    p_mir_operand p_src;
+    p_mir_symbol p_des;
 };
 
 struct mir_call_instr{
     p_mir_symbol p_func;
     p_mir_param_list p_param_list;
-    p_mir_symbol des;
+    p_mir_symbol p_des;
 };
 
 struct mir_array_instr{
     p_mir_symbol p_array;
-    p_mir_operand p_offest;
-    p_mir_symbol des;
+    p_mir_operand p_offset;
+    p_mir_symbol p_des;
 };
 
 struct mir_ret_instr{
