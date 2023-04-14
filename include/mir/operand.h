7d4b9ad30086f9c68205e3ea9bdfcd702e953285
zengyong <2595650269@qq.com>
2023-04-14 21:39:35 +0800

    refactor: put sym_kind and op_kind to mir.h

--- include/mir/operand.h
+++ include/mir/operand.h
@@ -2,12 +2,12 @@
 #define __MIRSYMBOL__
 
 #include <mir.h>
-typedef enum{
+enum mir_operand_kind{
     int_val,
     float_val,
     void_val, // 主要用做函数返回值 
     sym,
-}mir_operand_kind;;
+};
 struct mir_operand{
     union{
         p_mir_symbol p_sym;
