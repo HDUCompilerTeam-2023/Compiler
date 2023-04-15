351b335d0aacec4b8150049b6ad9374e4872e537
zengyong <2595650269@qq.com>
2023-04-15 21:00:39 +0800

    refactor:change mir_func gen and add block

--- src/mir_gen/func.c
+++ src/mir_gen/func.c
@@ -1,15 +1,20 @@
 #include <mir_gen/func.h>
 #include <mir_gen.h>
 
-p_mir_func mir_func_gen(p_symbol_sym p_func_sym, p_mir_basic_block p_block)
-{
-    p_mir_func p_func = malloc(sizeof(*p_func_sym));
+p_mir_func mir_func_gen(p_symbol_sym p_func_sym){
+    p_mir_func p_func = malloc(sizeof(*p_func));
     *p_func = (mir_func){
         .p_func_sym = p_func_sym,
-        .p_basic_block = p_block,
+        .p_basic_block = NULL,
         .node = list_head_init(&p_func->node),
         .temp_id = 0,
     };
+    return p_func;
+}
+
+p_mir_func mir_func_set_block(p_mir_func p_func, p_mir_basic_block p_block)
+{
+    p_func->p_basic_block = p_block;
     return p_func;
 }
 
