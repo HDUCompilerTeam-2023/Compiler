4e408312380f6aba45d6a3f9a1e69a4cd744f124
zengyong <2595650269@qq.com>
2023-04-15 17:05:08 +0800

    refactor: add block id

--- src/mir_gen/basic_block.c
+++ src/mir_gen/basic_block.c
@@ -1,20 +1,20 @@
+
 #include <mir_gen.h>
 #include <mir_gen/basic_block.h>
 
-static size_t num;
-p_mir_basic_block mir_basic_block_gen(void)
+p_mir_basic_block mir_basic_block_gen(p_mir_func p_func)
 {
     p_mir_basic_block p_mir_block = malloc(sizeof(*p_mir_block));
     *p_mir_block = (mir_basic_block){
         .instr_list = list_init_head(&p_mir_block->instr_list),
+        .block_prev = list_init_head(&p_mir_block->block_prev),
+        .block_id = p_func->temp_id ++,
     };
-    num = 0;// 初始化指令编号
     return p_mir_block;
 }
 
 p_mir_basic_block mir_basic_block_addinstr(p_mir_basic_block p_basic_block, p_mir_instr p_instr)
 {
-    p_instr->index  = num ++;
     list_add_prev(&p_instr->node, &p_basic_block->instr_list);
     return p_basic_block;
 }
@@ -27,5 +27,4 @@ void mir_basic_block_drop(p_mir_basic_block p_basic_block)
         mir_instr_drop(p_instr);
     }
     free(p_basic_block);
-    num = 0;
 }
