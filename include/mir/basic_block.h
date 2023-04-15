5c3453b7f6e1b4c46c88c3d4323a2a73a4ca7d56
zengyong <2595650269@qq.com>
2023-04-15 17:05:08 +0800

    refactor: add block id

--- include/mir/basic_block.h
+++ include/mir/basic_block.h
@@ -2,10 +2,11 @@
 #define __MIR_BASIC_BLOCK
 
 #include "mir.h"
-// 基本块, 之后补充
+
 struct mir_basic_block{
     list_head instr_list;
-    // TODO: 基本块间跳转
+    list_head block_prev;
+    size_t block_id;
 };
 
 #endif
--- include/mir_gen/basic_block.h
+++ include/mir_gen/basic_block.h
@@ -2,7 +2,7 @@
 #define __MIR_GEN_BASIC_BLOCK__
 #include <mir/basic_block.h>
 //p_mir_basic_block mir_basic_block_gen(void);
-p_mir_basic_block mir_basic_block_gen(void);
+p_mir_basic_block mir_basic_block_gen(p_mir_func p_func);
 
 p_mir_basic_block mir_basic_block_addinstr(p_mir_basic_block p_basic_block, p_mir_instr p_instr);
 void mir_basic_block_drop(p_mir_basic_block p_basic_block);
