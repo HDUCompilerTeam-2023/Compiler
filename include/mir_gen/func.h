70df9c24ebc65d48e396b6ac5f951b8398598cc2
zengyong <2595650269@qq.com>
2023-04-15 21:00:39 +0800

    refactor:change mir_func gen and add block

--- include/mir_gen/func.h
+++ include/mir_gen/func.h
@@ -1,7 +1,9 @@
 #ifndef __MIR_GEN_FUNC__
 #define __MIR_GEN_FUNC__
 #include <mir/func.h>
-p_mir_func mir_func_gen(p_symbol_sym p_func_sym, p_mir_basic_block p_block);
+
+p_mir_func mir_func_gen(p_symbol_sym p_sym);
+p_mir_func mir_func_set_block(p_mir_func p_func, p_mir_basic_block p_block);
 
 void mir_func_drop(p_mir_func p_func);
 #endif
