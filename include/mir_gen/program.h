fd432adc722219da66146d67907b9374b75d6e61
zengyong <2595650269@qq.com>
2023-04-15 20:40:37 +0800

    style: change two program function name

--- include/mir_gen/program.h
+++ include/mir_gen/program.h
@@ -3,8 +3,8 @@
 #include <mir/program.h>
 p_mir_program mir_program_gen(void);
 
-p_mir_program mir_func_add(p_mir_program p_program, p_mir_func p_func);
-p_mir_program mir_global_set(p_mir_program p_program, p_list_head p_head);
+p_mir_program mir_program_func_add(p_mir_program p_program, p_mir_func p_func);
+p_mir_program mir_program_global_set(p_mir_program p_program, p_list_head p_head);
 
 void mir_program_drop(p_mir_program p_program);
 
