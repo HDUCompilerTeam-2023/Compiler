442f602eb0c12090d012757699490f574cf903e4
zengyong <2595650269@qq.com>
2023-04-16 00:04:45 +0800

    factor:delete temp_id in mir func

--- include/mir/func.h
+++ include/mir/func.h
@@ -4,7 +4,6 @@
 struct mir_func{
     p_mir_basic_block p_basic_block;
     p_symbol_sym p_func_sym;
-    size_t temp_id; // 临时变量数量
 
     list_head node;
 };
--- include/mir_gen/basic_block.h
+++ include/mir_gen/basic_block.h
@@ -1,8 +1,9 @@
 #ifndef __MIR_GEN_BASIC_BLOCK__
 #define __MIR_GEN_BASIC_BLOCK__
 #include <mir/basic_block.h>
+#include <stddef.h>
 //p_mir_basic_block mir_basic_block_gen(void);
-p_mir_basic_block mir_basic_block_gen(p_mir_func p_func);
+p_mir_basic_block mir_basic_block_gen(size_t block_id);
 
 p_mir_basic_block mir_basic_block_addinstr(p_mir_basic_block p_basic_block, p_mir_instr p_instr);
 void mir_basic_block_drop(p_mir_basic_block p_basic_block);
