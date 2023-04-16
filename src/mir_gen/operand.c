830e6a6111225259f72466284a0f472fca8cd37b
zengyong <2595650269@qq.com>
2023-04-16 20:48:41 +0800

    feat: add fucntion to transform basic_type to sym_type

--- src/mir_gen/operand.c
+++ src/mir_gen/operand.c
@@ -2,7 +2,6 @@
 #include <mir_gen.h>
 #include <mir_gen/operand.h>
 
-
 p_mir_operand mir_operand_int_gen(int intconst)
 {
     p_mir_operand p_mir_int = malloc(sizeof(*p_mir_int));
@@ -61,6 +60,16 @@ p_mir_operand mir_operand_temp_sym_gen(size_t id, p_symbol_type p_type)
         .p_type = p_type,
     };
     return p_sym;
+}
+// 临时变量在一些情况下需要将 立即数的基本类型转换为 type
+p_symbol_type mir_operand_sym_type_gen(basic_type b_type)
+{
+    p_symbol_type p_type = malloc(sizeof(*p_type));
+    *p_type = (symbol_type){
+        .kind = type_var,
+        .basic = b_type,
+    };
+    return p_type;
 }
 
 void mir_operand_drop(p_mir_operand p_operand)
