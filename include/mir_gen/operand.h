a635fefa2b56048c2ce366b7563340fc2dbbe4ae
zengyong <2595650269@qq.com>
2023-04-16 22:34:57 +0800

    feat: add mir operand void gen

--- include/mir_gen/operand.h
+++ include/mir_gen/operand.h
@@ -4,6 +4,8 @@
 #include <mir/operand.h>
 p_mir_operand mir_operand_int_gen(int intconst);
 p_mir_operand mir_operand_float_gen(float floatconst);
+p_mir_operand mir_operand_void_gen(void);
+
 p_mir_operand mir_operand_declared_sym_gen(p_symbol_sym p_mir_sym);
 p_mir_operand mir_operand_temp_sym_gen(size_t id, p_symbol_type p_type);
 
