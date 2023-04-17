9f00f686ba6987d72d742978a14f8cd708a4323e
zengyong <2595650269@qq.com>
2023-04-17 21:19:58 +0800

    feat: add hir2mir_info

---
+++ src/hir2mir/info_gen.c
@@ -0,0 +1,17 @@
+#include <hir2mir/info_gen.h>
+
+p_hir2mir_info hir2mir_info_gen(){
+    p_hir2mir_info p_info = malloc(sizeof(*p_info));
+    *p_info = (hir2mir_info){
+        .id = 1,
+        .p_current_basic_block = NULL,
+        .p_operand_list = mir_operand_list_gen(),
+        .p_basic_block_list = NULL,
+    };
+    return p_info;
+}
+
+void hir2mir_info_drop(p_hir2mir_info p_info)
+{
+    free(p_info);
+}
