1ca4b1accb9fcaa7e52479da7d8ae26a1b825bc3
zengyong <2595650269@qq.com>
2023-04-17 21:25:32 +0800

    refactor: add hir2mir.h

---
+++ include/hir2mir.h
@@ -0,0 +1,14 @@
+#ifndef __HIR2MIR__
+#define  __HIR2MIR__
+#include <hir2mir/info_gen.h>
+
+#include <hir2mir/program.h>
+#include <hir2mir/func.h>
+#include <hir2mir/block.h>
+#include <hir2mir/param.h>
+#include <hir2mir/stmt.h>
+#include <hir2mir/exp.h>
+#include <hir2mir/operand.h>
+
+
+#endif
