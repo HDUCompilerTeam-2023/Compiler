8a73b3009f8bda164b247e00962797f9f0035fd4
zengyong <2595650269@qq.com>
2023-04-18 21:43:33 +0800

    refactor: return stmt goto same block

--- include/hir2mir/info_gen.h
+++ include/hir2mir/info_gen.h
@@ -8,9 +8,11 @@ struct hir2mir_info{
     p_mir_basic_block p_current_basic_block;
     p_mir_operand_list p_operand_list;
     p_mir_basic_block_list p_basic_block_list;
+    p_mir_basic_block p_ret_block;// 返回的块
+    p_mir_operand p_ret_operand;// 返回值所存储的位置
 };
 
-p_hir2mir_info hir2mir_info_gen(void);
+p_hir2mir_info hir2mir_info_gen(p_symbol_sym p_func_sym);
 p_hir2mir_info hir2mir_info_set_current_block(p_mir_basic_block p_block);
 p_mir_basic_block hir2mir_info_get_current_block(p_hir2mir_info p_info);
 
--- src/hir2mir/info_gen.c
+++ src/hir2mir/info_gen.c
@@ -1,13 +1,19 @@
 #include <hir2mir/info_gen.h>
-
-p_hir2mir_info hir2mir_info_gen(){
+// 根据函数信息 生成接下来需要收集的信息的返回值类型
+p_hir2mir_info hir2mir_info_gen(p_symbol_sym p_func_sym){
     p_hir2mir_info p_info = malloc(sizeof(*p_info));
     *p_info = (hir2mir_info){
-        .id = 1,
+        .id = 0,
         .p_current_basic_block = NULL,
         .p_operand_list = mir_operand_list_gen(),
         .p_basic_block_list = mir_basic_block_list_gen(),
+        .p_ret_block = mir_basic_block_gen(0),
     };
+    p_info->p_ret_operand = mir_operand_temp_sym_basic_gen(p_info->id ++, p_func_sym->p_type->basic);
+    mir_operand_list_add(p_info->p_operand_list, p_info->p_ret_operand);
+    p_mir_instr p_ret = mir_ret_instr_gen( p_info->p_ret_operand);
+    mir_basic_block_addinstr(p_info->p_ret_block, p_ret);
+    mir_basic_block_list_add(p_info->p_basic_block_list, p_info->p_ret_block);
     return p_info;
 }
 
