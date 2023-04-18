8364db4452a51d612260a13b4ed2dd43235c4000
zengyong <2595650269@qq.com>
2023-04-18 16:45:39 +0800

    refactor: change operand list to func

--- src/hir2mir/program.c
+++ src/hir2mir/program.c
@@ -5,14 +5,11 @@ p_mir_program hir2mir_program_gen(p_hir_program p_h_program)
 {
     p_mir_program p_m_program = mir_program_gen();
     mir_program_global_set(p_m_program, &p_h_program->pss->global);
-    p_hir2mir_info p_info = hir2mir_info_gen();
     p_list_head p_node;
     list_for_each(p_node, &p_h_program->pss->def_function){
         p_symbol_sym p_func_sym = list_entry(p_node, symbol_sym, node);
-        p_mir_func p_func = hir2mir_func_gen(p_info, p_func_sym);
+        p_mir_func p_func = hir2mir_func_gen(p_func_sym);
         mir_program_func_add(p_m_program,  p_func);
     }
-    p_m_program->p_operand_list = p_info->p_operand_list;
-    hir2mir_info_drop(p_info);
     return p_m_program;
 }
--- src/mir_gen/func.c
+++ src/mir_gen/func.c
@@ -8,6 +8,7 @@ p_mir_func mir_func_gen(p_symbol_sym p_func_sym){
         .p_basic_block = NULL,
         .node = list_head_init(&p_func->node),
         .p_basic_block_list = NULL,
+        .p_operand_list = NULL,
     };
     return p_func;
 }
@@ -22,5 +23,6 @@ void mir_func_drop(p_mir_func p_func)
 {
     assert(p_func);
     mir_basic_block_list_drop(p_func->p_basic_block_list);
+    mir_operand_list_drop(p_func->p_operand_list);
     free(p_func);
 }
