37790d07af524dbd0b0a254976195471ca0668e4
zengyong <2595650269@qq.com>
2023-04-18 19:29:35 +0800

    feat: add mir port declaration

---
+++ include/mir_port/func.h
@@ -0,0 +1,8 @@
+#ifndef __MIR_PORT_FUNC__
+#define __MIR_PORT_FUNC__
+#include <mir.h>
+p_mir_basic_block mir_func_get_basic_block_entry(p_mir_func p_func);
+
+// 获取函数 symbol 信息
+p_symbol_sym mir_func_get_sym(p_mir_func p_func);
+#endif
