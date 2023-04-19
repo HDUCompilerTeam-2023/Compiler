51e174dd5c48959357e84cc5821dc8b9f3b4da76
zengyong <2595650269@qq.com>
2023-04-19 11:06:08 +0800

    refactor: set block id after produce

--- src/hir2mir/block.c
+++ src/hir2mir/block.c
@@ -18,7 +18,7 @@ p_mir_basic_block hir2mir_block_gen(p_hir2mir_info p_info, p_hir_block p_block)
 // 生成 空 basic_block 并将其添加到 函数的 basic_block_list 中
 p_mir_basic_block hir2mir_basic_block_gen(p_hir2mir_info p_info)
 {
-    p_mir_basic_block p_new_basic_block = mir_basic_block_gen(p_info->id ++);
+    p_mir_basic_block p_new_basic_block = mir_basic_block_gen();
     mir_basic_block_list_add(p_info->p_basic_block_list, p_new_basic_block);
     return p_new_basic_block;
 }
