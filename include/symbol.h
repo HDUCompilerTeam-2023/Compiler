f1d746db0b912367f2a446393c552dd9767e40de
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-04-22 11:23:26 +0800

    feat: store basic type for init val

--- include/symbol.h
+++ include/symbol.h
@@ -4,6 +4,7 @@
 #include <util.h>
 
 typedef struct symbol_store symbol_store, *p_symbol_store;
+typedef struct symbol_init_val symbol_init_val, *p_symbol_init_val;
 typedef struct symbol_init symbol_init, *p_symbol_init;
 typedef struct symbol_sym symbol_sym, *p_symbol_sym;
 typedef struct symbol_str symbol_str, *p_symbol_str;
