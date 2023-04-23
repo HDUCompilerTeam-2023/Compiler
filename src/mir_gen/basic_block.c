1515ffbea1dfcf1ceb1b36a7598eb9abb8a168ec
zengyong <2595650269@qq.com>
2023-04-23 16:58:38 +0800

    feat: delete basic_block_list

--- src/mir_gen/basic_block.c
+++ src/mir_gen/basic_block.c
@@ -8,7 +8,7 @@ p_mir_basic_block mir_basic_block_gen()
     p_mir_basic_block p_mir_block = malloc(sizeof(*p_mir_block));
     *p_mir_block = (mir_basic_block){
         .instr_list = list_head_init(&p_mir_block->instr_list),
-        .p_prev_block_list = mir_basic_block_list_gen(),
+        .prev_basic_block_list = list_head_init(&p_mir_block->prev_basic_block_list),
         .block_id = 0,
         .p_next = NULL,
     };
@@ -17,28 +17,15 @@ p_mir_basic_block mir_basic_block_gen()
 // 插入前驱节点列表
 p_mir_basic_block mir_basic_block_add_prev(p_mir_basic_block p_prev, p_mir_basic_block p_next)
 {
-    mir_basic_block_list_add(p_next->p_prev_block_list, p_prev);
+    p_mir_basic_block_list_node node = malloc(sizeof(*node));
+    *node = (mir_basic_block_list_node){
+        .p_basic_block = p_prev,
+        .node = list_head_init(&node->node),
+    };
+    list_add_prev(&node->node, &p_next->prev_basic_block_list);
     return p_next;
 }
 
-p_mir_basic_block_list mir_basic_block_list_gen(void)
-{
-    p_mir_basic_block_list p_basic_block_list = malloc(sizeof(*p_basic_block_list));
-    p_basic_block_list->basic_block_list = list_head_init(&p_basic_block_list->basic_block_list);
-    return p_basic_block_list;
-}
-
-p_mir_basic_block_list mir_basic_block_list_add(p_mir_basic_block_list p_basic_block_list,p_mir_basic_block p_basic_block)
-{
-    assert(p_basic_block);
-    p_mir_basic_block_list_node p_basic_block_list_node = malloc(sizeof(*p_basic_block_list_node));
-    *p_basic_block_list_node = (mir_basic_block_list_node){
-        .p_basic_block = p_basic_block,
-        .node = list_head_init(&p_basic_block_list_node->node),
-    };
-    list_add_prev(&p_basic_block_list_node->node, &p_basic_block_list->basic_block_list);
-    return p_basic_block_list;
-}
 
 p_mir_basic_block mir_basic_block_addinstr(p_mir_basic_block p_basic_block, p_mir_instr p_instr)
 {
@@ -56,23 +43,10 @@ void mir_basic_block_drop(p_mir_basic_block p_basic_block)
         list_del(&p_instr->node);
         mir_instr_drop(p_instr);
     }
-    while (!list_head_alone(&p_basic_block->p_prev_block_list->basic_block_list)) {
-        p_mir_basic_block_list_node p_basic_block_list_node = list_entry(p_basic_block->p_prev_block_list->basic_block_list.p_next, mir_basic_block_list_node, node);
+    while (!list_head_alone(&p_basic_block->prev_basic_block_list)) {
+        p_mir_basic_block_list_node p_basic_block_list_node = list_entry(p_basic_block->prev_basic_block_list.p_next, mir_basic_block_list_node, node);
         list_del(&p_basic_block_list_node->node);
         free(p_basic_block_list_node);
     }
-    free(p_basic_block->p_prev_block_list);
     free(p_basic_block);
-}
-
-void mir_basic_block_list_drop(p_mir_basic_block_list p_basic_block_list)
-{
-    assert(p_basic_block_list);
-    while (!list_head_alone(&p_basic_block_list->basic_block_list)) {
-        p_mir_basic_block_list_node p_basic_block_list_node = list_entry(p_basic_block_list->basic_block_list.p_next, mir_basic_block_list_node, node);
-        list_del(&p_basic_block_list_node->node);
-        mir_basic_block_drop(p_basic_block_list_node->p_basic_block);
-        free(p_basic_block_list_node);
-    }
-    free(p_basic_block_list);
 }
