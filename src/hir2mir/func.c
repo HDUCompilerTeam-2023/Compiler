bae255b63a3239297595560bb7746120d1808833
zengyong <2595650269@qq.com>
2023-04-24 16:24:52 +0800

    feat: put func into info

--- src/hir2mir/func.c
+++ src/hir2mir/func.c
@@ -7,10 +7,9 @@
 p_mir_func hir2mir_func_gen(p_hir_func p_h_func)
 {
     p_mir_func p_m_func = mir_func_gen(p_h_func->p_sym);
-    p_hir2mir_info p_info = hir2mir_info_gen(p_h_func->p_sym);
-    p_info->p_func = p_m_func;
-    p_mir_basic_block p_block = hir2mir_block_gen(p_info, p_h_func->p_block);
-    mir_func_set_block(p_m_func, p_block);
+    p_hir2mir_info p_info = hir2mir_info_gen(p_m_func);
+    p_m_func->p_basic_block = hir2mir_block_gen(p_info, p_h_func->p_block);
+
     p_info->p_ret_operand->p_temp_sym->id = p_info->temp_id ++;
     mir_func_temp_sym_add(p_m_func, p_info->p_ret_operand->p_temp_sym);
     // 为 return 语句设置编号
