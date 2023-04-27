61c97ab1899331490f592a16a75fa395e801835f
zengyong <2595650269@qq.com>
2023-04-27 10:33:03 +0800

    feat: 加入mir_manager 并求解支配树

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <frontend.h>
 #include <hir2mir.h>
+#include <mir_manager.h>
 
 #include <hir_print.h>
 #include <mir_print.h>
@@ -12,7 +13,10 @@ int main(int argc, char *argv[])
 		p_hir_program p_hir = frontend_trans(argv[i]);
 		hir_program_print(p_hir);
 		p_mir_program p_mir = hir2mir_program_gen(p_hir);
+		mir_cfg_set_program_dom(p_mir);
 		mir_program_print(p_mir);
+
+		mir_program_dom_info_print(p_mir);
 		mir_program_drop(p_mir);
 	}
 	return 0;
