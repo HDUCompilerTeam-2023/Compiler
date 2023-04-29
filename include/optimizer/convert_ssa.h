cc2727f56ca875979a5982013f6a1892e3a0344f
zengyong <2595650269@qq.com>
2023-04-29 22:26:13 +0800

    feat: 插入 phi 函数（基本块参数）

--- include/optimizer/convert_ssa.h
+++ include/optimizer/convert_ssa.h
@@ -6,12 +6,22 @@
 struct convert_ssa{
     p_mir_basic_block p_basic_block; 
     p_bitmap dom_frontier; // 支配边界
+
+    p_bitmap p_phi_var; // phi 函数变量集合
+    p_bitmap p_def_var; // 定值集合
+
+    bool if_in; // 是否在工作表中
 };
 typedef struct convert_ssa convert_ssa, *p_convert_ssa;
 
-void convert_ssa_gen(convert_ssa *dfs_seq, size_t block_num, p_mir_basic_block p_basic_block, size_t current_num);
-size_t convert_ssa_init_dfs_sequence(convert_ssa *dfs_seq, size_t block_num, p_mir_basic_block p_entry, size_t current_num);
+void convert_ssa_gen(convert_ssa *dfs_seq, size_t block_num, size_t var_num, p_mir_basic_block p_basic_block, size_t current_num);
+size_t convert_ssa_init_dfs_sequence(convert_ssa *dfs_seq, size_t var_num, size_t block_num, p_mir_basic_block p_entry, size_t current_num);
+void convert_ssa_init_var_list(p_mir_operand p_var_list, size_t var_num, p_mir_func p_func, p_mir_temp_sym p_ret);
+
 void convert_ssa_compute_dom_frontier(convert_ssa *dfs_seq, size_t block_num);
+
+void convert_ssa_insert_phi(p_convert_ssa dfs_seq, size_t block_num, p_mir_temp_sym p_ret, size_t var_num);
+void convert_ssa_rewrite_phi(p_convert_ssa dfs_seq, size_t block_num, p_mir_operand p_var_list, size_t var_num);
 
 void convert_ssa_func(p_mir_func p_func);
 void convert_ssa_program(p_mir_program p_program);
