cbdb2dc85eb2d2dfb166e478bc0b708b2878db12
zengyong <2595650269@qq.com>
2023-04-30 18:18:36 +0800

    feat: 实现变量重命名

--- include/optimizer/convert_ssa.h
+++ include/optimizer/convert_ssa.h
@@ -42,6 +42,8 @@ void convert_ssa_compute_dom_frontier(convert_ssa *dfs_seq, size_t block_num);
 void convert_ssa_insert_phi(p_convert_ssa dfs_seq, size_t block_num, p_ssa_var_list_info p_var_list);
 void convert_ssa_rewrite_phi(p_convert_ssa dfs_seq, size_t block_num, p_ssa_var_info p_var_list, size_t var_num);
 
+void convert_ssa_rename_var(p_ssa_var_list_info p_var_list, p_mir_basic_block p_entry);
+
 void convert_ssa_func(p_mir_func p_func, p_mir_program p_program);
 void convert_ssa_program(p_mir_program p_program);
 
