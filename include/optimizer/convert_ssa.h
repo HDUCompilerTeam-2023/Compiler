1a94a126a5e5b3bc2c839b2274187d2237724ce2
zengyong <2595650269@qq.com>
2023-04-30 18:12:39 +0800

    feat: 加入变量列表以加入全局变量

--- include/optimizer/convert_ssa.h
+++ include/optimizer/convert_ssa.h
@@ -2,6 +2,10 @@
 #define __OPTIMIZER__CONVERT_SSA__
 
 #include <mir_gen.h>
+typedef struct convert_ssa convert_ssa, *p_convert_ssa;
+typedef struct operand_stack_node operand_stack_node, *p_operand_stack_node;
+typedef struct ssa_var_info ssa_var_info, *p_ssa_var_info;
+typedef struct ssa_var_list_info ssa_var_list_info, *p_ssa_var_list_info;
 
 struct convert_ssa{
     p_mir_basic_block p_basic_block; 
@@ -19,23 +23,29 @@ struct ssa_var_info{
     size_t current_count;
     size_t count;
 };
-typedef struct convert_ssa convert_ssa, *p_convert_ssa;
-typedef struct operand_stack_node operand_stack_node, *p_operand_stack_node;
-typedef struct ssa_var_info ssa_var_info, *p_ssa_var_info;
+
+struct ssa_var_list_info{
+    p_ssa_var_info p_base;
+    size_t global_num;
+    size_t local_num;
+    size_t temp_num;
+    p_mir_temp_sym p_ret_sym;
+};
+
 
 void convert_ssa_gen(convert_ssa *dfs_seq, size_t block_num, size_t var_num, p_mir_basic_block p_basic_block, size_t current_num);
 size_t convert_ssa_init_dfs_sequence(convert_ssa *dfs_seq, size_t var_num, size_t block_num, p_mir_basic_block p_entry, size_t current_num);
-void convert_ssa_init_var_list(p_ssa_var_info p_var_list, size_t var_num, p_mir_func p_func, p_mir_temp_sym p_ret);
+p_ssa_var_list_info convert_ssa_init_var_list(p_mir_func p_func, p_mir_program p_program);
 
 void convert_ssa_compute_dom_frontier(convert_ssa *dfs_seq, size_t block_num);
 
-void convert_ssa_insert_phi(p_convert_ssa dfs_seq, size_t block_num, p_mir_temp_sym p_ret, size_t var_num);
+void convert_ssa_insert_phi(p_convert_ssa dfs_seq, size_t block_num, p_ssa_var_list_info p_var_list);
 void convert_ssa_rewrite_phi(p_convert_ssa dfs_seq, size_t block_num, p_ssa_var_info p_var_list, size_t var_num);
 
-void convert_ssa_func(p_mir_func p_func);
+void convert_ssa_func(p_mir_func p_func, p_mir_program p_program);
 void convert_ssa_program(p_mir_program p_program);
 
 void convert_ssa_dfs_seq_drop(convert_ssa *dfs_seq, size_t block_num);
-void ssa_var_info_drop(p_ssa_var_info p_info, size_t var_num);
+void ssa_var_list_info_drop(p_ssa_var_list_info p_info);
 
 #endif
