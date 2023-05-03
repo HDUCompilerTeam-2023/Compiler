1943b60fd52c94eec5dacb7820463e0e9616931c
ZmeetL <1113989231@qq.com>
2023-05-03 21:02:37 +0800

    refactor: 整合mir_port 到头文件

---
+++ include/mir_port.h
@@ -0,0 +1,11 @@
+#ifndef __MIR_PORT__
+#define __MIR_PORT__
+
+#include <mir_port/basic_block.h>
+#include <mir_port/func.h>
+#include <mir_port/instr.h>
+#include <mir_port/operand.h>
+#include <mir_port/param.h>
+#include <mir_port/program.h>
+
+#endif
