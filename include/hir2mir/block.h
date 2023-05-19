e18373f70ab9d8c907293c82a8fcbb6af8c30364
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-05-19 13:48:10 +0800

    refactor: add first block in info gen

--- include/hir2mir/block.h
+++ include/hir2mir/block.h
@@ -2,6 +2,6 @@
 #define __HIR2MIR__BLOCK__
 #include <hir2mir/info_gen.h>
 
-p_mir_basic_block hir2mir_block_gen(p_hir2mir_info p_info, p_hir_block p_block);
+void hir2mir_block_gen(p_hir2mir_info p_info, p_hir_block p_block);
 
 #endif
