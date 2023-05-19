af1128bdb2c4bc7517b148ae6974d5c6dff1a9fe
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-05-19 14:25:41 +0800

    feat: 改变 mir 函数参数的传递方式

--- include/mir/func.h
+++ include/mir/func.h
@@ -5,6 +5,9 @@
 struct mir_func {
     p_symbol_sym p_func_sym;
 
+    p_mir_vreg *param_vreg;
+    size_t param_vreg_cnt;
+
     list_head entry_block;
     list_head vreg_list;
     list_head vmem_list;
