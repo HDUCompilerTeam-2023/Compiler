d3de9d5e9c9e18d4dd3ef879cc17e3803d63482a
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-05-22 16:32:03 +0800

    style: delete mir_port

--- include/mir_port/param.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MIR_PORT_PARAM__
-#define __MIR_PORT_PARAM__
-#include <mir.h>
-
-// 获取参数的操作数
-p_mir_operand mir_param_get_operand(p_mir_param p_param);
-p_list_head mir_param_list_get_head(p_mir_param_list p_mir_param_list);
-
-#endif
