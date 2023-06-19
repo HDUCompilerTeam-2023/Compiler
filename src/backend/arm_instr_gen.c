cfe5d9188bb00ac93250e3b7c512360743e1b7de
zengyong <2595650269@qq.com>
2023-06-19 19:23:23 +0800

    feat: 加入判断寄存器是否合法

--- src/backend/arm_instr_gen.c
+++ src/backend/arm_instr_gen.c
@@ -9,7 +9,7 @@ static char regs[48][4] = { "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
     "s28", "s29", "s30", "s31" };
 
 static const size_t R_NUM = 16;
-// static const size_t S_NUM = 32;
+static const size_t S_NUM = 32;
 
 char *uint32_str(uint32_t intconst) {
     if (!intconst) {
@@ -47,6 +47,7 @@ static inline void operand2str(char *asm_code, size_t operand, size_t lsl_imme, 
         uint32_str_cat(asm_code, operand);
         return;
     }
+    assert(operand < R_NUM);
     strcat(asm_code, regs[operand]);
     if (lsl_imme) {
         strcat(asm_code, ", lsl #");
@@ -81,6 +82,8 @@ static inline void arm_cond_type_gen(char *asm_code, arm_cond_type cond_type) {
 }
 
 void arm_data_process_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rs, size_t operand, bool s, size_t lsl_imme, bool if_imme) {
+    assert(rd < R_NUM);
+    assert(rs < R_NUM);
     switch (type) {
     case arm_add:
         strcat(asm_code, "   add");
@@ -128,6 +131,8 @@ void arm_data_process_gen(char *asm_code, arm_instr_type type, size_t rd, size_t
 
 // if_imme 表示 operand 是否是立即数
 void arm_load_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rn, size_t offset, size_t lsl_imme, bool if_imme) {
+    assert(rd < R_NUM);
+    assert(rn < R_NUM);
     strcat(asm_code, "   ldr ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", [");
@@ -140,6 +145,8 @@ void arm_load_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rn, siz
 }
 
 void arm_store_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rn, size_t offset, size_t lsl_imme, bool if_imme) {
+    assert(rd < R_NUM);
+    assert(rn < R_NUM);
     strcat(asm_code, "   str ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", [");
@@ -152,6 +159,7 @@ void arm_store_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rn, si
 }
 
 void arm_compare_gen(char *asm_code, arm_instr_type type, size_t rd, size_t operand, size_t lsl_imme, bool if_imme) {
+    assert(rd < R_NUM);
     switch (type) {
     case arm_cmp:
         strcat(asm_code, "   cmp ");
@@ -175,6 +183,7 @@ void arm_compare_gen(char *asm_code, arm_instr_type type, size_t rd, size_t oper
 }
 
 void arm_mov16_gen(char *asm_code, arm_instr_type type, size_t rd, size_t imme16, bool s) {
+    assert(rd < R_NUM);
     switch (type) {
     case arm_movw:
         strcat(asm_code, "   movw ");
@@ -194,6 +203,7 @@ void arm_mov16_gen(char *asm_code, arm_instr_type type, size_t rd, size_t imme16
 }
 
 void arm_mov_gen(char *asm_code, arm_instr_type type, size_t rd, size_t operand, bool s, size_t lsl_imme, bool if_imme) {
+    assert(rd < R_NUM);
     switch (type) {
     case arm_mov:
         strcat(asm_code, "   mov");
@@ -214,6 +224,7 @@ void arm_mov_gen(char *asm_code, arm_instr_type type, size_t rd, size_t operand,
 }
 
 void arm_movcond_gen(char *asm_code, arm_cond_type cond_type, size_t rd, size_t operand, size_t lsl_imme, bool if_imme) {
+    assert(rd < R_NUM);
     strcat(asm_code, "   mov");
     arm_cond_type_gen(asm_code, cond_type);
     strcat(asm_code, regs[rd]);
@@ -223,6 +234,9 @@ void arm_movcond_gen(char *asm_code, arm_cond_type cond_type, size_t rd, size_t 
 }
 
 void arm_mul_gen(char *asm_code, size_t rd, size_t rm, size_t rs, bool s) {
+    assert(rd < R_NUM);
+    assert(rm < R_NUM);
+    assert(rs < R_NUM);
     strcat(asm_code, "   mul");
     if (s)
         strcat(asm_code, "s");
@@ -236,6 +250,9 @@ void arm_mul_gen(char *asm_code, size_t rd, size_t rm, size_t rs, bool s) {
 }
 
 void arm_sdiv_gen(char *asm_code, size_t rd, size_t rm, size_t rs) {
+    assert(rd < R_NUM);
+    assert(rm < R_NUM);
+    assert(rs < R_NUM);
     strcat(asm_code, "  sdiv ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", ");
@@ -251,6 +268,7 @@ void arm_label_gen(char *asm_code, char *name) {
 }
 
 void arm_jump_reg_gen(char *asm_code, arm_instr_type type, arm_cond_type cond_type, size_t r_target) {
+    assert(r_target < R_NUM);
     switch (type) {
     case arm_bx:
         strcat(asm_code, "   bx");
@@ -341,9 +359,11 @@ void arm_func_sym_declare_gen(char *asm_code, char *p_func) {
 
 void arm_push_gen(char *asm_code, size_t *reg_id, size_t num) {
     if (num == 0) return;
+    assert(reg_id[0] < R_NUM);
     strcat(asm_code, "   push {");
     strcat(asm_code, regs[reg_id[0]]);
     for (size_t i = 1; i < num; i++) {
+        assert(reg_id[i] < R_NUM);
         strcat(asm_code, ", ");
         strcat(asm_code, regs[reg_id[i]]);
     }
@@ -352,9 +372,11 @@ void arm_push_gen(char *asm_code, size_t *reg_id, size_t num) {
 
 void arm_pop_gen(char *asm_code, size_t *reg_id, size_t num) {
     if (num == 0) return;
+    assert(reg_id[0] < R_NUM);
     strcat(asm_code, "   pop {");
     strcat(asm_code, regs[reg_id[0]]);
     for (size_t i = 1; i < num; i++) {
+        assert(reg_id[i] < R_NUM);
         strcat(asm_code, ", ");
         strcat(asm_code, regs[reg_id[i]]);
     }
@@ -362,6 +384,7 @@ void arm_pop_gen(char *asm_code, size_t *reg_id, size_t num) {
 }
 
 void arm_get_global_addr(char *asm_code, size_t rd, char *name) {
+    assert(rd < R_NUM);
     strcat(asm_code, "   ldr ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", =");
@@ -369,8 +392,9 @@ void arm_get_global_addr(char *asm_code, size_t rd, char *name) {
     strcat(asm_code, "\n");
 }
 
-
 void arm_vset_flag(char *asm_code, size_t r) {
+    assert(r >= R_NUM);
+    assert(r < R_NUM + S_NUM);
     strcat(asm_code, "   vcmp.f32 ");
     strcat(asm_code, regs[r]);
     strcat(asm_code, ", #0\n");
@@ -394,6 +418,10 @@ void arm_vmov_gen(char *asm_code, size_t rd, size_t rs) {
 }
 
 void arm_vneg_gen(char *asm_code, size_t rd, size_t rs) {
+    assert(rs >= R_NUM);
+    assert(rs < R_NUM + S_NUM);
+    assert(rd >= R_NUM);
+    assert(rd < R_NUM + S_NUM);
     strcat(asm_code, "   vneg.f32 ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", ");
@@ -402,6 +430,10 @@ void arm_vneg_gen(char *asm_code, size_t rd, size_t rs) {
 }
 
 void arm_vcvt_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rs) {
+    assert(rd >= R_NUM);
+    assert(rd < R_NUM + S_NUM);
+    assert(rs >= R_NUM);
+    assert(rs < R_NUM + S_NUM);
     strcat(asm_code, "   vcvt");
     switch (type) {
     case arm_int2float:
@@ -420,6 +452,12 @@ void arm_vcvt_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rs) {
 }
 
 void arm_vdata_process_gen(char *asm_code, arm_instr_type type, size_t rd, size_t rs1, size_t rs2) {
+    assert(rd >= R_NUM);
+    assert(rd < R_NUM + S_NUM);
+    assert(rs1 >= R_NUM);
+    assert(rs1 < R_NUM + S_NUM);
+    assert(rs2 >= R_NUM);
+    assert(rs2 < R_NUM + S_NUM);
     switch (type) {
     case arm_add:
         strcat(asm_code, "   vadd");
@@ -449,6 +487,10 @@ void arm_vdata_process_gen(char *asm_code, arm_instr_type type, size_t rd, size_
 }
 
 void arm_vcompare_gen(char *asm_code, arm_instr_type type, size_t rs1, size_t rs2) {
+    assert(rs1 >= R_NUM);
+    assert(rs1 < R_NUM + S_NUM);
+    assert(rs2 >= R_NUM);
+    assert(rs2 < R_NUM + S_NUM);
     switch (type) {
     case arm_cmp:
         strcat(asm_code, "  vcmp");
@@ -470,8 +512,10 @@ void arm_vcompare_gen(char *asm_code, arm_instr_type type, size_t rs1, size_t rs
 
 void arm_vload_gen(char *asm_code, size_t rd, size_t rs, size_t offset, bool if_imme) {
     assert(rd >= R_NUM);
+    assert(rd < R_NUM + S_NUM);
     assert(rs < R_NUM);
-    assert(if_imme || offset < R_NUM);
+    if (!if_imme)
+        assert(offset < R_NUM);
     strcat(asm_code, "   vldr.32 ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", [");
@@ -491,8 +535,10 @@ void arm_vload_gen(char *asm_code, size_t rd, size_t rs, size_t offset, bool if_
 
 void arm_vstore_gen(char *asm_code, size_t rd, size_t rs, size_t offset, bool if_imme) {
     assert(rd >= R_NUM);
+    assert(rd < R_NUM + S_NUM);
     assert(rs < R_NUM);
-    assert(if_imme || offset < R_NUM);
+    if (!if_imme)
+        assert(offset < R_NUM);
     strcat(asm_code, "   vstr.32 ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", [");
@@ -512,9 +558,13 @@ void arm_vstore_gen(char *asm_code, size_t rd, size_t rs, size_t offset, bool if
 
 void arm_vpush_gen(char *asm_code, size_t *reg_id, size_t num) {
     if (num == 0) return;
+    assert(reg_id[0] >= R_NUM);
+    assert(reg_id[0] < R_NUM + S_NUM);
     strcat(asm_code, "   vpush {");
     strcat(asm_code, regs[reg_id[0]]);
     for (size_t i = 1; i < num; i++) {
+        assert(reg_id[i] >= R_NUM);
+        assert(reg_id[i] < R_NUM + S_NUM);
         strcat(asm_code, ", ");
         strcat(asm_code, regs[reg_id[i]]);
     }
@@ -522,9 +572,13 @@ void arm_vpush_gen(char *asm_code, size_t *reg_id, size_t num) {
 }
 void arm_vpop_gen(char *asm_code, size_t *reg_id, size_t num) {
     if (num == 0) return;
+    assert(reg_id[0] >= R_NUM);
+    assert(reg_id[0] < R_NUM + S_NUM);
     strcat(asm_code, "   vpop {");
     strcat(asm_code, regs[reg_id[0]]);
     for (size_t i = 1; i < num; i++) {
+        assert(reg_id[i] >= R_NUM);
+        assert(reg_id[i] < R_NUM + S_NUM);
         strcat(asm_code, ", ");
         strcat(asm_code, regs[reg_id[i]]);
     }
@@ -532,6 +586,8 @@ void arm_vpop_gen(char *asm_code, size_t *reg_id, size_t num) {
 }
 
 void arm_get_float_label_val(char *asm_code, size_t rd, char *func_name, size_t len) {
+    assert(rd >= R_NUM);
+    assert(rd < R_NUM + S_NUM);
     strcat(asm_code, "   vldr.32 ");
     strcat(asm_code, regs[rd]);
     strcat(asm_code, ", .");
