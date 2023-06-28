dce6071e7c53f8a5b220751f5bea218eb632241a
zengyong <2595650269@qq.com>
2023-06-28 16:12:40 +0800

    feat: 输出汇编到文件

--- src/main.c
+++ src/main.c
@@ -14,11 +14,52 @@
 
 #include <backend/arm/codegen.h>
 #include <stdio.h>
+
+static inline char *get_prefix(char *file) {
+    char *output = malloc(sizeof(*file) * (strlen(file) * 2));
+    char *p1 = output;
+    char *p2 = file;
+    while (*p2 != '.' && *p2 != '\0') {
+        *p1 = *p2;
+        p1++;
+        p2++;
+    }
+    *(p1) = '\0';
+    return output;
+}
+
+static inline void asm2file(char *output_file, char *asm_code) {
+    FILE *file = fopen(output_file, "w");
+    fputs(asm_code, file);
+    fclose(file);
+}
+
 int main(int argc, char *argv[]) {
     if (argc == 1)
         argv[argc++] = NULL;
     for (int i = 1; i < argc; ++i) {
         char asm_code[100000] = "";
+        char *output_file = NULL;
+        int j = i;
+        strcat(asm_code, ".file \"");
+        if (i + 1 < argc && !strcmp(argv[i + 1], "-o")) {
+            output_file = malloc(strlen(argv[i + 2]) + 1);
+            strcpy(output_file, argv[i + 2]);
+            strcat(asm_code, argv[i]);
+            j += 2;
+        }
+        else {
+            if (argv[i]) { // 默认目标与文件路径一致
+                output_file = strcat(get_prefix(argv[i]), ".s");
+                strcat(asm_code, argv[i]);
+            }
+            else { // 从输入读取
+                output_file = malloc(sizeof(*output_file) * 20);
+                strcpy(output_file, "./output.s");
+                strcat(asm_code, "input");
+            }
+        }
+        strcat(asm_code, "\"\n");
         // gen ir
         p_program p_program = frontend_trans(argv[i]);
         program_variable_print(p_program);
@@ -57,6 +98,9 @@ int main(int argc, char *argv[]) {
         printf("%s", asm_code);
         // drop ir
         program_drop(p_program);
+        asm2file(output_file, asm_code);
+        i = j;
+        free(output_file);
     }
     return 0;
 }
