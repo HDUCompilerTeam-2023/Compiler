c11edde5c6dc49242f8982415fdbb5357ea926c6
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-07-03 21:43:48 +0800

    feat: allow print location in frontend error

--- src/frontend/log.c
+++ src/frontend/log.c
@@ -2,9 +2,32 @@
 
 #include <stdarg.h>
 #include <util/log.h>
+#include <stdio.h>
+
+int yylloc2str(char *yyo, YYLTYPE *yylocp) {
+    int res = 0;
+    int end_col = 0 != yylocp->last_column ? yylocp->last_column - 1 : 0;
+    if (0 <= yylocp->first_line) {
+        res += sprintf (yyo + res, "%d", yylocp->first_line);
+        if (0 <= yylocp->first_column)
+            res += sprintf (yyo + res, ".%d", yylocp->first_column);
+    }
+    if (0 <= yylocp->last_line) {
+        if (yylocp->first_line < yylocp->last_line) {
+            res += sprintf (yyo + res, "-%d", yylocp->last_line);
+            if (0 <= end_col)
+                res += sprintf (yyo + res, ".%d", end_col);
+        }
+        else if (0 <= end_col && yylocp->first_column < end_col)
+            res += sprintf (yyo + res, "-%d", end_col);
+    }
+    yyo[res++] = '\0';
+    return res;
+}
 
 void frontend_log(loglevel level, YYLTYPE *yylloc, yyscan_t yyscanner, const char *format, ...) {
-    const char *pos = "Line 1";
+    char pos[50];
+    yylloc2str(pos, yylloc);
 
     va_list args;
     va_start(args, format);
