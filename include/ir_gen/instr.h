fda69bd5316c5486009ad113375116ccf0a1089e
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-07-09 15:04:53 +0800

    feat: add function to reset ir_instr

--- include/ir_gen/instr.h
+++ include/ir_gen/instr.h
@@ -7,10 +7,16 @@ p_ir_instr ir_unary_instr_gen(ir_unary_op op, p_ir_operand p_src, p_ir_vreg p_de
 
 p_ir_instr ir_call_instr_gen(p_symbol_func p_func, p_ir_param_list p_param_list, p_ir_vreg p_des);
 
-p_ir_instr ir_alloca_instr_gen(p_symbol_var p_vmem, p_ir_vreg p_des);
 p_ir_instr ir_gep_instr_gen(p_ir_operand p_addr, p_ir_operand p_offset, p_ir_vreg p_des, bool is_element);
 p_ir_instr ir_load_instr_gen(p_ir_operand p_addr, p_ir_vreg p_des, bool is_stack_ptr);
 p_ir_instr ir_store_instr_gen(p_ir_operand p_addr, p_ir_operand p_src, bool is_stack_ptr);
+
+void ir_instr_reset_binary(p_ir_instr p_instr, ir_binary_op op, p_ir_operand p_src1, p_ir_operand p_src2, p_ir_vreg p_des);
+void ir_instr_reset_unary(p_ir_instr p_instr, ir_unary_op op, p_ir_operand p_src, p_ir_vreg p_des);
+void ir_instr_reset_call(p_ir_instr p_instr, p_symbol_func p_func, p_ir_param_list p_param_list, p_ir_vreg p_des);
+void ir_instr_reset_gep(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_operand p_offset, p_ir_vreg p_des, bool is_element);
+void ir_instr_reset_load(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_vreg p_des, bool is_stack_ptr);
+void ir_instr_reset_store(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_operand p_src, bool is_stack_ptr);
 
 p_ir_operand ir_instr_get_src1(p_ir_instr p_instr);
 p_ir_operand ir_instr_get_src2(p_ir_instr p_instr);
--- src/ir_gen/instr.c
+++ src/ir_gen/instr.c
@@ -1,9 +1,56 @@
 #include <ir_gen.h>
 #include <ir_gen/instr.h>
 
-p_ir_instr ir_binary_instr_gen(ir_binary_op op, p_ir_operand p_src1, p_ir_operand p_src2, p_ir_vreg p_des) {
-    p_ir_instr p_instr = NULL;
-    p_instr = malloc(sizeof(*p_instr));
+static inline void _ir_instr_inner_drop(p_ir_instr p_instr) {
+    switch (p_instr->irkind) {
+    case ir_binary:
+        ir_operand_drop(p_instr->ir_binary.p_src1);
+        ir_operand_drop(p_instr->ir_binary.p_src2);
+        ir_vreg_set_instr_def(p_instr->ir_binary.p_des, NULL);
+        break;
+    case ir_unary:
+        ir_operand_drop(p_instr->ir_unary.p_src);
+        ir_vreg_set_instr_def(p_instr->ir_unary.p_des, NULL);
+        break;
+    case ir_call:
+        ir_param_list_drop(p_instr->ir_call.p_param_list);
+        if (p_instr->ir_call.p_des)
+            ir_vreg_set_instr_def(p_instr->ir_call.p_des, NULL);
+        break;
+    case ir_gep:
+        ir_operand_drop(p_instr->ir_gep.p_addr);
+        ir_operand_drop(p_instr->ir_gep.p_offset);
+        ir_vreg_set_instr_def(p_instr->ir_gep.p_des, NULL);
+        break;
+    case ir_store:
+        ir_operand_drop(p_instr->ir_store.p_addr);
+        ir_operand_drop(p_instr->ir_store.p_src);
+        break;
+    case ir_load:
+        ir_operand_drop(p_instr->ir_load.p_addr);
+        ir_vreg_set_instr_def(p_instr->ir_load.p_des, NULL);
+        break;
+    }
+}
+void ir_instr_drop(p_ir_instr p_instr) {
+    assert(p_instr);
+    list_del(&p_instr->node);
+    _ir_instr_inner_drop(p_instr);
+    ir_bb_phi_list_drop(p_instr->p_live_in);
+    ir_bb_phi_list_drop(p_instr->p_live_out);
+    free(p_instr);
+}
+
+static inline void _ir_instr_init(p_ir_instr p_instr) {
+    *p_instr = (ir_instr) {
+        .node = list_head_init(&p_instr->node),
+        .instr_id = 0,
+        .p_live_in = ir_bb_phi_list_init(),
+        .p_live_out = ir_bb_phi_list_init(),
+    };
+}
+
+static inline void _ir_instr_binary_set(p_ir_instr p_instr, ir_binary_op op, p_ir_operand p_src1, p_ir_operand p_src2, p_ir_vreg p_des) {
     *p_instr = (ir_instr) {
         .irkind = ir_binary,
         .ir_binary = (ir_binary_instr) {
@@ -12,18 +59,25 @@ p_ir_instr ir_binary_instr_gen(ir_binary_op op, p_ir_operand p_src1, p_ir_operan
             .p_src2 = p_src2,
             .p_des = p_des,
         },
-        .node = list_head_init(&p_instr->node),
-        .instr_id = 0,
-        .p_live_in = ir_bb_phi_list_init(),
-        .p_live_out = ir_bb_phi_list_init(),
+        .node = p_instr->node,
+        .instr_id = p_instr->instr_id,
+        .p_live_in = p_instr->p_live_in,
+        .p_live_out = p_instr->p_live_out,
     };
     ir_vreg_set_instr_def(p_des, p_instr);
+}
+void ir_instr_reset_binary(p_ir_instr p_instr, ir_binary_op op, p_ir_operand p_src1, p_ir_operand p_src2, p_ir_vreg p_des) {
+    _ir_instr_inner_drop(p_instr);
+    _ir_instr_binary_set(p_instr, op, p_src1, p_src2, p_des);
+}
+p_ir_instr ir_binary_instr_gen(ir_binary_op op, p_ir_operand p_src1, p_ir_operand p_src2, p_ir_vreg p_des) {
+    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+    _ir_instr_init(p_instr);
+    _ir_instr_binary_set(p_instr, op, p_src1, p_src2, p_des);
     return p_instr;
 }
 
-p_ir_instr ir_unary_instr_gen(ir_unary_op op, p_ir_operand p_src, p_ir_vreg p_des) {
-    p_ir_instr p_instr = NULL;
-    p_instr = malloc(sizeof(*p_instr));
+static inline void _ir_instr_unary_set(p_ir_instr p_instr, ir_unary_op op, p_ir_operand p_src, p_ir_vreg p_des) {
     *p_instr = (ir_instr) {
         .irkind = ir_unary,
         .ir_unary = (ir_unary_instr) {
@@ -31,18 +85,25 @@ p_ir_instr ir_unary_instr_gen(ir_unary_op op, p_ir_operand p_src, p_ir_vreg p_de
             .p_src = p_src,
             .p_des = p_des,
         },
-        .node = list_head_init(&p_instr->node),
-        .instr_id = 0,
-        .p_live_in = ir_bb_phi_list_init(),
-        .p_live_out = ir_bb_phi_list_init(),
+        .node = p_instr->node,
+        .instr_id = p_instr->instr_id,
+        .p_live_in = p_instr->p_live_in,
+        .p_live_out = p_instr->p_live_out,
     };
     ir_vreg_set_instr_def(p_des, p_instr);
+}
+void ir_instr_reset_unary(p_ir_instr p_instr, ir_unary_op op, p_ir_operand p_src, p_ir_vreg p_des) {
+    _ir_instr_inner_drop(p_instr);
+    _ir_instr_unary_set(p_instr, op, p_src, p_des);
+}
+p_ir_instr ir_unary_instr_gen(ir_unary_op op, p_ir_operand p_src, p_ir_vreg p_des) {
+    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+    _ir_instr_init(p_instr);
+    _ir_instr_unary_set(p_instr, op, p_src, p_des);
     return p_instr;
 }
 
-p_ir_instr ir_call_instr_gen(p_symbol_func p_func, p_ir_param_list p_param_list, p_ir_vreg p_des) {
-    p_ir_instr p_instr = malloc(sizeof(*p_instr));
-
+static inline void _ir_instr_call_set(p_ir_instr p_instr, p_symbol_func p_func, p_ir_param_list p_param_list, p_ir_vreg p_des) {
     *p_instr = (ir_instr) {
         .irkind = ir_call,
         .ir_call = (ir_call_instr) {
@@ -50,18 +111,26 @@ p_ir_instr ir_call_instr_gen(p_symbol_func p_func, p_ir_param_list p_param_list,
             .p_des = p_des,
             .p_param_list = p_param_list,
         },
-        .node = list_head_init(&p_instr->node),
-        .instr_id = 0,
-        .p_live_in = ir_bb_phi_list_init(),
-        .p_live_out = ir_bb_phi_list_init(),
+        .node = p_instr->node,
+        .instr_id = p_instr->instr_id,
+        .p_live_in = p_instr->p_live_in,
+        .p_live_out = p_instr->p_live_out,
     };
     if (p_des)
         ir_vreg_set_instr_def(p_des, p_instr);
+}
+void ir_instr_reset_call(p_ir_instr p_instr, p_symbol_func p_func, p_ir_param_list p_param_list, p_ir_vreg p_des) {
+    _ir_instr_inner_drop(p_instr);
+    _ir_instr_call_set(p_instr, p_func, p_param_list, p_des);
+}
+p_ir_instr ir_call_instr_gen(p_symbol_func p_func, p_ir_param_list p_param_list, p_ir_vreg p_des) {
+    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+    _ir_instr_init(p_instr);
+    _ir_instr_call_set(p_instr, p_func, p_param_list, p_des);
     return p_instr;
 }
 
-p_ir_instr ir_gep_instr_gen(p_ir_operand p_addr, p_ir_operand p_offset, p_ir_vreg p_des, bool is_element) {
-    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+static inline void _ir_instr_gep_set(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_operand p_offset, p_ir_vreg p_des, bool is_element) {
     *p_instr = (ir_instr) {
         .irkind = ir_gep,
         .ir_gep = (ir_gep_instr) {
@@ -70,17 +139,25 @@ p_ir_instr ir_gep_instr_gen(p_ir_operand p_addr, p_ir_operand p_offset, p_ir_vre
             .p_offset = p_offset,
             .is_element = is_element,
         },
-        .node = list_head_init(&p_instr->node),
-        .instr_id = 0,
-        .p_live_in = ir_bb_phi_list_init(),
-        .p_live_out = ir_bb_phi_list_init(),
+        .node = p_instr->node,
+        .instr_id = p_instr->instr_id,
+        .p_live_in = p_instr->p_live_in,
+        .p_live_out = p_instr->p_live_out,
     };
     ir_vreg_set_instr_def(p_des, p_instr);
+}
+void ir_instr_reset_gep(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_operand p_offset, p_ir_vreg p_des, bool is_element) {
+    _ir_instr_inner_drop(p_instr);
+    _ir_instr_gep_set(p_instr, p_addr, p_offset, p_des, is_element);
+}
+p_ir_instr ir_gep_instr_gen(p_ir_operand p_addr, p_ir_operand p_offset, p_ir_vreg p_des, bool is_element) {
+    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+    _ir_instr_init(p_instr);
+    _ir_instr_gep_set(p_instr, p_addr, p_offset, p_des, is_element);
     return p_instr;
 }
 
-p_ir_instr ir_load_instr_gen(p_ir_operand p_addr, p_ir_vreg p_des, bool is_stack_ptr) {
-    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+static inline void _ir_instr_load_set(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_vreg p_des, bool is_stack_ptr) {
     *p_instr = (ir_instr) {
         .irkind = ir_load,
         .ir_load = (ir_load_instr) {
@@ -88,17 +165,25 @@ p_ir_instr ir_load_instr_gen(p_ir_operand p_addr, p_ir_vreg p_des, bool is_stack
             .p_des = p_des,
             .is_stack_ptr = is_stack_ptr,
         },
-        .node = list_head_init(&p_instr->node),
-        .instr_id = 0,
-        .p_live_in = ir_bb_phi_list_init(),
-        .p_live_out = ir_bb_phi_list_init(),
+        .node = p_instr->node,
+        .instr_id = p_instr->instr_id,
+        .p_live_in = p_instr->p_live_in,
+        .p_live_out = p_instr->p_live_out,
     };
     ir_vreg_set_instr_def(p_des, p_instr);
+}
+void ir_instr_reset_load(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_vreg p_des, bool is_stack_ptr) {
+    _ir_instr_inner_drop(p_instr);
+    _ir_instr_load_set(p_instr, p_addr, p_des, is_stack_ptr);
+}
+p_ir_instr ir_load_instr_gen(p_ir_operand p_addr, p_ir_vreg p_des, bool is_stack_ptr) {
+    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+    _ir_instr_init(p_instr);
+    _ir_instr_load_set(p_instr, p_addr, p_des, is_stack_ptr);
     return p_instr;
 }
 
-p_ir_instr ir_store_instr_gen(p_ir_operand p_addr, p_ir_operand p_src, bool is_stack_ptr) {
-    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+static inline void _ir_instr_store_set(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_operand p_src, bool is_stack_ptr) {
     *p_instr = (ir_instr) {
         .irkind = ir_store,
         .ir_store = (ir_store_instr) {
@@ -106,11 +191,20 @@ p_ir_instr ir_store_instr_gen(p_ir_operand p_addr, p_ir_operand p_src, bool is_s
             .p_src = p_src,
             .is_stack_ptr = is_stack_ptr,
         },
-        .node = list_head_init(&p_instr->node),
-        .instr_id = 0,
-        .p_live_in = ir_bb_phi_list_init(),
-        .p_live_out = ir_bb_phi_list_init(),
+        .node = p_instr->node,
+        .instr_id = p_instr->instr_id,
+        .p_live_in = p_instr->p_live_in,
+        .p_live_out = p_instr->p_live_out,
     };
+}
+void ir_instr_reset_store(p_ir_instr p_instr, p_ir_operand p_addr, p_ir_operand p_src, bool is_stack_ptr) {
+    _ir_instr_inner_drop(p_instr);
+    _ir_instr_store_set(p_instr, p_addr, p_src, is_stack_ptr);
+}
+p_ir_instr ir_store_instr_gen(p_ir_operand p_addr, p_ir_operand p_src, bool is_stack_ptr) {
+    p_ir_instr p_instr = malloc(sizeof(*p_instr));
+    _ir_instr_init(p_instr);
+    _ir_instr_store_set(p_instr, p_addr, p_src, is_stack_ptr);
     return p_instr;
 }
 
@@ -171,34 +265,3 @@ p_ir_operand ir_instr_get_store_addr(p_ir_instr p_instr) {
     }
 }
 
-// 操作数已经被存在单独的列表中
-void ir_instr_drop(p_ir_instr p_instr) {
-    assert(p_instr);
-    list_del(&p_instr->node);
-    switch (p_instr->irkind) {
-    case ir_binary:
-        ir_operand_drop(p_instr->ir_binary.p_src1);
-        ir_operand_drop(p_instr->ir_binary.p_src2);
-        break;
-    case ir_unary:
-        ir_operand_drop(p_instr->ir_unary.p_src);
-        break;
-    case ir_call:
-        ir_param_list_drop(p_instr->ir_call.p_param_list);
-        break;
-    case ir_gep:
-        ir_operand_drop(p_instr->ir_gep.p_addr);
-        ir_operand_drop(p_instr->ir_gep.p_offset);
-        break;
-    case ir_store:
-        ir_operand_drop(p_instr->ir_store.p_addr);
-        ir_operand_drop(p_instr->ir_store.p_src);
-        break;
-    case ir_load:
-        ir_operand_drop(p_instr->ir_load.p_addr);
-        break;
-    }
-    ir_bb_phi_list_drop(p_instr->p_live_in);
-    ir_bb_phi_list_drop(p_instr->p_live_out);
-    free(p_instr);
-}
