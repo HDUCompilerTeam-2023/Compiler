a83ceb8f8d0c4a221fabb38e11281971d60d6136
HuJJ-NB <91231458+HuJJ-NB@users.noreply.github.com>
2023-07-19 21:24:36 +0800

    feat: simple copy propagation

---
+++ include/ir_opt/copy_propagation.h
@@ -0,0 +1,8 @@
+#ifndef __IR_OPT_COPY_PROPAGATION__
+#define __IR_OPT_COPY_PROPAGATION__
+
+#include <program/use.h>
+
+void ir_opt_copy_propagation(p_program p_ir);
+
+#endif
---
+++ src/ir_opt/copy_propagation.c
@@ -0,0 +1,41 @@
+#include <program/use.h>
+#include <program/def.h>
+#include <symbol_gen/func.h>
+#include <ir/basic_block.h>
+#include <ir_gen/instr.h>
+#include <ir/vreg.h>
+#include <ir_gen/operand.h>
+
+void ir_opt_copy_propagation(p_program p_ir) {
+    p_list_head p_node;
+    list_for_each(p_node, &p_ir->function) {
+        p_symbol_func p_func = list_entry(p_node, symbol_func, node);
+        p_list_head p_node;
+        list_for_each(p_node, &p_func->block) {
+            p_ir_basic_block p_bb = list_entry(p_node, ir_basic_block, node);
+            p_list_head p_node, p_next;
+            list_for_each_safe(p_node, p_next, &p_bb->instr_list) {
+                p_ir_instr p_instr = list_entry(p_node, ir_instr, node);
+                if (p_instr->irkind != ir_unary)
+                    continue;
+                if (p_instr->ir_unary.op != ir_val_assign)
+                    continue;
+                p_ir_operand p_src_op = p_instr->ir_unary.p_src;
+                if (p_src_op->kind != reg)
+                    continue;
+                assert(p_src_op->p_vreg);
+                p_ir_vreg p_src = p_src_op->p_vreg;
+                p_ir_vreg p_des = p_instr->ir_unary.p_des;
+                p_list_head p_node, p_next;
+                list_for_each_safe(p_node, p_next, &p_des->use_list) {
+                    p_ir_operand p_use = list_entry(p_node, ir_operand, use_node);
+                    assert(p_use->kind == reg);
+                    ir_operand_reset_vreg(p_use, p_src);
+                }
+                ir_instr_drop(p_instr);
+                symbol_func_vreg_del(p_func, p_des);
+            }
+        }
+    }
+}
+
