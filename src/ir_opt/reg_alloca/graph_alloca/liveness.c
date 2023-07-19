e406ec279c10ce6cfec1cee9969db793570b6e6a
zengyong <2595650269@qq.com>
2023-07-19 19:06:29 +0800

    feat: 活跃性分析时使用变量使用列表提升效率

--- src/ir_opt/reg_alloca/graph_alloca/liveness.c
+++ src/ir_opt/reg_alloca/graph_alloca/liveness.c
@@ -3,11 +3,11 @@
 #include <ir_gen.h>
 #include <symbol_gen.h>
 
-static void p_live_in_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
-static void p_live_out_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
-static void p_live_out_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
-static void p_live_in_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
-static void p_live_in_branch(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
+static inline void p_live_in_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_vreg p_vreg);
+static inline void p_live_out_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_vreg p_vreg);
+static inline void p_live_out_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
+static inline void p_live_in_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
+static inline void p_live_in_branch(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg);
 
 static inline void set_graph_table_edge(p_liveness_info p_info, p_ir_vreg r1, p_ir_vreg r2) {
     p_info->graph_table[r1->id][r2->id] = p_info->graph_table[r2->id][r1->id] = true;
@@ -23,7 +23,8 @@ static inline bool in_bb_phi_list(p_liveness_info p_info, p_ir_basic_block p_bas
     return false;
 }
 
-static void p_live_in_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
+static inline void p_live_in_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_vreg p_vreg) {
+    p_ir_basic_block p_basic_block = p_instr->p_basic_block;
     bitmap_add_element(p_info->instr_live_in[p_instr->instr_id], p_vreg->id);
     if (&p_instr->node == p_basic_block->instr_list.p_next) {
         if (in_bb_phi_list(p_info, p_basic_block, p_vreg))
@@ -32,13 +33,7 @@ static void p_live_in_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir
     }
     else {
         p_ir_instr p_last_instr = list_entry(p_instr->node.p_prev, ir_instr, node);
-        p_live_out_statments(p_info, p_last_instr, p_basic_block, p_vreg);
-    }
-}
-static inline void p_live_in_statments_(p_liveness_info p_info, p_ir_instr p_instr, p_ir_basic_block p_basic_block, p_ir_operand p_operand) {
-    if (p_operand->kind == reg) {
-        symbol_func_basic_block_init_visited(p_info->p_func);
-        p_live_in_statments(p_info, p_instr, p_basic_block, p_operand->p_vreg);
+        p_live_out_statments(p_info, p_last_instr, p_vreg);
     }
 }
 
@@ -77,20 +72,20 @@ static inline p_ir_vreg add_edge_with(p_ir_instr p_instr) {
     }
 }
 
-static void p_live_out_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
+static inline void p_live_out_statments(p_liveness_info p_info, p_ir_instr p_instr, p_ir_vreg p_vreg) {
     bitmap_add_element(p_info->instr_live_out[p_instr->instr_id], p_vreg->id);
     p_ir_vreg p_des = add_edge_with(p_instr);
     if (p_des) {
         if (p_des != p_vreg) {
             set_graph_table_edge(p_info, p_vreg, p_des);
-            p_live_in_statments(p_info, p_instr, p_basic_block, p_vreg);
+            p_live_in_statments(p_info, p_instr, p_vreg);
         }
     }
     else
-        p_live_in_statments(p_info, p_instr, p_basic_block, p_vreg);
+        p_live_in_statments(p_info, p_instr, p_vreg);
 }
 
-static void p_live_in_branch(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
+static inline void p_live_in_branch(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
     bitmap_add_element(p_info->block_branch_live_in[p_basic_block->block_id], p_vreg->id);
     if (list_head_alone(&p_basic_block->instr_list)) {
         if (in_bb_phi_list(p_info, p_basic_block, p_vreg))
@@ -99,18 +94,11 @@ static void p_live_in_branch(p_liveness_info p_info, p_ir_basic_block p_basic_bl
     }
     else {
         p_ir_instr p_last_instr = list_entry(p_basic_block->instr_list.p_prev, ir_instr, node);
-        p_live_out_statments(p_info, p_last_instr, p_basic_block, p_vreg);
+        p_live_out_statments(p_info, p_last_instr, p_vreg);
     }
 }
 
-static void p_live_in_branch_(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_operand p_operand) {
-    if (p_operand->kind == reg) {
-        symbol_func_basic_block_init_visited(p_info->p_func);
-        p_live_in_branch(p_info, p_basic_block, p_operand->p_vreg);
-    }
-}
-
-static void p_live_out_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
+static inline void p_live_out_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
     if (p_basic_block->if_visited)
         return;
     p_basic_block->if_visited = true;
@@ -118,7 +106,7 @@ static void p_live_out_block(p_liveness_info p_info, p_ir_basic_block p_basic_bl
     p_live_in_branch(p_info, p_basic_block, p_vreg);
 }
 
-void p_live_in_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
+static inline void p_live_in_block(p_liveness_info p_info, p_ir_basic_block p_basic_block, p_ir_vreg p_vreg) {
     bitmap_add_element(p_info->block_live_in[p_basic_block->block_id], p_vreg->id);
     p_list_head p_node;
     if (p_info->p_func->block.p_next == &p_basic_block->node) {
@@ -144,7 +132,7 @@ static inline void set_live(p_liveness_info p_info, p_ir_vreg_list p_list, p_bit
 }
 
 // 将 info中的 bitmap 转为链表
-static void set_func_live(p_liveness_info p_info) {
+static inline void set_func_live(p_liveness_info p_info) {
     p_list_head p_block_node;
     list_for_each(p_block_node, &p_info->p_func->block) {
         p_ir_basic_block p_block = list_entry(p_block_node, ir_basic_block, node);
@@ -228,67 +216,36 @@ void liveness_info_drop(p_liveness_info p_info) {
     free(p_info->p_vregs);
     free(p_info);
 }
+static inline void deal_vreg(p_liveness_info p_info, p_ir_vreg p_vreg) {
+    symbol_func_basic_block_init_visited(p_info->p_func);
+    p_list_head p_use_node;
+    list_for_each(p_use_node, &p_vreg->use_list) {
+        p_ir_operand p_operand = list_entry(p_use_node, ir_operand, use_node);
+        switch (p_operand->used_type) {
+        case instr_ptr:
+            p_live_in_statments(p_info, p_operand->p_instr, p_vreg);
+            break;
+        case bb_param_ptr:
+            p_live_in_branch(p_info, p_operand->p_bb_param->p_target->p_source_block, p_vreg);
+            break;
+        case cond_ptr:
+            break;
+        case ret_ptr:
+            p_live_in_branch(p_info, p_operand->p_basic_block, p_vreg);
+            break;
+        }
+    }
+}
 // 活跃性分析，如果用变量的使用列表效率可以更高
 void liveness_analysis(p_liveness_info p_info) {
-    p_list_head p_block_node;
-    list_for_each(p_block_node, &p_info->p_func->block) {
-        p_ir_basic_block p_basic_block = list_entry(p_block_node, ir_basic_block, node);
-        p_list_head p_instr_node;
-        p_list_head p_node;
-        list_for_each(p_instr_node, &p_basic_block->instr_list) {
-            p_ir_instr p_instr = list_entry(p_instr_node, ir_instr, node);
-            switch (p_instr->irkind) {
-            case ir_unary:
-                p_live_in_statments_(p_info, p_instr, p_basic_block, p_instr->ir_unary.p_src);
-                break;
-            case ir_binary:
-                p_live_in_statments_(p_info, p_instr, p_basic_block, p_instr->ir_binary.p_src1);
-                p_live_in_statments_(p_info, p_instr, p_basic_block, p_instr->ir_binary.p_src2);
-                break;
-            case ir_load:
-                p_live_in_statments_(p_info, p_instr, p_basic_block, p_instr->ir_load.p_addr);
-                break;
-            case ir_store:
-                p_live_in_statments_(p_info, p_instr, p_basic_block, p_instr->ir_store.p_addr);
-                p_live_in_statments_(p_info, p_instr, p_basic_block, p_instr->ir_store.p_src);
-                break;
-            case ir_call:
-                list_for_each(p_node, &p_instr->ir_call.param_list) {
-                    p_ir_param p_param = list_entry(p_node, ir_param, node);
-                    if (!p_param->is_in_mem)
-                        p_live_in_statments_(p_info, p_instr, p_basic_block, p_param->p_param);
-                }
-                break;
-            case ir_gep:
-                assert(0);
-            }
-        }
-        switch (p_basic_block->p_branch->kind) {
-        case ir_br_branch:
-            list_for_each(p_node, &p_basic_block->p_branch->p_target_1->block_param) {
-                p_ir_operand p_param = list_entry(p_node, ir_bb_param, node)->p_bb_param;
-                p_live_in_branch_(p_info, p_basic_block, p_param);
-            }
-            break;
-        case ir_cond_branch:
-            // 条件是不活跃的
-            // p_live_out_statments_(p_info, p_last_instr, p_basic_block, p_basic_block->p_branch->p_exp);
-            list_for_each(p_node, &p_basic_block->p_branch->p_target_1->block_param) {
-                p_ir_operand p_param = list_entry(p_node, ir_bb_param, node)->p_bb_param;
-                p_live_in_branch_(p_info, p_basic_block, p_param);
-            }
-            list_for_each(p_node, &p_basic_block->p_branch->p_target_2->block_param) {
-                p_ir_operand p_param = list_entry(p_node, ir_bb_param, node)->p_bb_param;
-                p_live_in_branch_(p_info, p_basic_block, p_param);
-            }
-            break;
-        case ir_ret_branch:
-            if (p_basic_block->p_branch->p_exp)
-                p_live_in_branch_(p_info, p_basic_block, p_basic_block->p_branch->p_exp);
-            break;
-        case ir_abort_branch:
-            break;
-        }
+    p_list_head p_vreg_node;
+    list_for_each(p_vreg_node, &p_info->p_func->param_reg_list) {
+        p_ir_vreg p_vreg = list_entry(p_vreg_node, ir_vreg, node);
+        deal_vreg(p_info, p_vreg);
+    }
+    list_for_each(p_vreg_node, &p_info->p_func->vreg_list) {
+        p_ir_vreg p_vreg = list_entry(p_vreg_node, ir_vreg, node);
+        deal_vreg(p_info, p_vreg);
     }
     set_func_live(p_info);
 }
