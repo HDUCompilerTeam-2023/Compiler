bd53b782b3d081149cd2931fe875961d2c933043
ZmeetL <1113989231@qq.com>
2023-07-22 21:06:33 +0800

    refactor: nestree build

--- include/util/structure.h
+++ include/util/structure.h
@@ -2,15 +2,29 @@
 #define __UTIL_STRUCTURE__
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
+typedef struct {
+    int stacktop;
+    int stacksize;
+
+    uint64_t *stackdata;
+} stack;
+
+stack *InitStack();
+void stack_push(stack *pushstack, uint64_t pushnumber);
+uint64_t stack_pop(stack *popstack);
+uint64_t stack_top(stack *popstack);
+bool checkstack(stack *checkstack);
+void destroystack(stack *stk);
 typedef enum Color {
     RED,
     BLACK
 } Color;
 
 typedef struct Node {
-    size_t key;
+    uint64_t key;
     Color color;
     struct Node *left;
     struct Node *right;
@@ -21,13 +35,13 @@ typedef struct RedBlackTree {
     Node *root;
 } RedBlackTree;
 
-Node *createNode(size_t key, Color color);
+Node *createNode(uint64_t key, Color color);
 RedBlackTree *initializeRedBlackTree();
 void leftRotate(RedBlackTree *tree, Node *x);
 void rightRotate(RedBlackTree *tree, Node *x);
 void insertFixup(RedBlackTree *tree, Node *z);
-bool insert(RedBlackTree *tree, size_t key);
-bool search(Node *root, size_t key);
+bool insert(RedBlackTree *tree, uint64_t key);
+bool search(Node *root, uint64_t key);
 void freeNode(Node *node);
 void clearRedBlackTreeNode(Node *root);
 void clearRedBlackTree(RedBlackTree *tree);
